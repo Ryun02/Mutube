dd5ef4c6762894fd09fc250ca52059928802e572
Ryun02 <dangryun22@gmail.com>
2022-03-09 22:13:37 +0900

    #3.0

--- src/server.js
+++ src/server.js
@@ -1,3 +1,9 @@
 import express from "express";
 
-console.log("i'm handsome");
+const PORT = 5228;
+
+const app = express();
+
+const handleListning = () => console.log(`Server Listening on port ${PORT}`);
+
+app.listen(PORT, handleListning);
