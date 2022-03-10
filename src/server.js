e03018ff1a53512743c3deff18fd4576c6d9c2d0
Ryun02 <dangryun22@gmail.com>
2022-03-10 22:48:41 +0900

    #3.2 ~ #3.11

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 import express from "express";
+import morgan from "morgan";
+
+const app = express();
+const logger = morgan("dev");
 
 const PORT = 5228;
 
-const app = express();
+const home = (req, res) => {
+  return res.send("I home");
+};
 
-const handleListning = () => console.log(`Server Listening on port ${PORT}`);
+app.use(logger);
+app.get("/", home);
+
+const handleListning = () =>
+  console.log(`Server listening on http://localhost:${PORT}`);
 
 app.listen(PORT, handleListning);
